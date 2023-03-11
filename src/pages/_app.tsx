2835a3ee5f29f95dd14c97ae24b3b4f9cb0c211a
takahirohimi <takahiro.mth.1209@gmail.com>
2023-03-11 21:21:57 +0900

    必要なパッケージをインストール＆グローバルスタイルのセットアップ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,37 @@
 import type { AppProps } from "next/app";
+import styled, {
+  createGlobalStyle,
+  css,
+  ThemeProvider,
+} from "styled-components";
+import cssBaseLine from "smarthr-normalize-css";
+import { createTheme, ThemeProvider as SHUIThemeProvider } from "smarthr-ui";
+
+export const smarthrUITheme = createTheme();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <SHUIThemeProvider theme={smarthrUITheme}>
+        <ThemeProvider theme={smarthrUITheme}>
+          <GlobalStyle />
+          <ContainerLayout>
+            <Component {...pageProps} />
+          </ContainerLayout>
+        </ThemeProvider>
+      </SHUIThemeProvider>
+    </>
+  );
 }
+
+const GlobalStyle = createGlobalStyle`
+  ${cssBaseLine}
+`;
+
+const ContainerLayout = styled.div(
+  ({ theme: { space, contentWidth } }) => css`
+    max-width: ${contentWidth};
+    margin-inline: auto;
+    padding: ${space(1.5)} ${space(2)} ${space(4)};
+  `
+);
