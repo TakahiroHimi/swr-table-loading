32008d084019c1b252a2608af4fd71a900ff808f
takahirohimi <takahiro.mth.1209@gmail.com>
2023-03-11 21:56:21 +0900

    apiから取得したデータをテーブルに表示する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,39 @@
+import { Base, Table, Td, Th } from "smarthr-ui";
+import useSWR from "swr";
+
+const fetcher = (url: string, method?: string) =>
+  fetch(url, { method }).then((res) => res.json());
+
 export default function Home() {
+  const { data, isLoading } = useSWR<{ id: string; name: string }[]>(
+    "/api/users",
+    fetcher
+  );
+
+  if (!data || isLoading) return <>is loading...</>;
+
   return (
     <>
-      <main>TEST</main>
+      <main>
+        <Base overflow={{ x: "hidden", y: undefined }}>
+          <Table>
+            <thead>
+              <tr>
+                <Th>ID</Th>
+                <Th>名前</Th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((user) => (
+                <tr key={user.id}>
+                  <Td>{user.id}</Td>
+                  <Td>{user.name}</Td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Base>
+      </main>
     </>
   );
 }
